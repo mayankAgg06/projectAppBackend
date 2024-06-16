34aa1a00f7d77801d24e7f0dfcd49ff21c693998
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-06-17 03:26:44 +0530

    changed value types

--- models/Opportunity.js
+++ models/Opportunity.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const OpportunitySchema = new Schema({
   profileName: { type: String, required: true },
   companyName: { type: String, required: true },
-  stipend: { type: String, required: true },
-  location: { type: String, required: true },
+  stipend: { type: Object, required: true },
+  location: { type: Array, required: true },
   duration: { type: String, required: true },
   startDate: { type: String, required: true },
 });
